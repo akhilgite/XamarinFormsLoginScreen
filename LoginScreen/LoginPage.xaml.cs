4e4c49c9abc17e0afe82dd0f024e701706cd837b
Akhil Gite <dexter@Akhils-MacBook-Pro.local>
2019-01-28 22:29:27 +0530

    init

---
+++ LoginScreen/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LoginScreen
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LoginScreen/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginScreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //for safe area in iOS
+            //https://blog.xamarin.com/making-ios-11-even-easier-xamarin-forms/
+        }
+    }
+}
